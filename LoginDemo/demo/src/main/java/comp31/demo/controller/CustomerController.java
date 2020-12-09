a58d2e293f7a4c5a94f0d67d42000a923fb0d851
Leif Lang <longliveleif@gmail.com>
2020-12-09 14:00:40 -0500

    Added to orderRepo

--- LoginDemo/demo/src/main/java/comp31/demo/controller/CustomerController.java
+++ LoginDemo/demo/src/main/java/comp31/demo/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package comp31.demo.controller;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import comp31.demo.model.User;
+import comp31.demo.model.order;
 import comp31.demo.repo.UserRepo;
 import comp31.demo.repo.ToppingRepo;
 import comp31.demo.repo.OrderRepo;
@@ -28,6 +30,8 @@ public class CustomerController
     public String getLogin(User user, Model model) 
     {
         User currentUser = (User)model.getAttribute("currentUser");
+        List<order> orderList = orderRepo.findByUserId(currentUser);
+        model.addAttribute("orderList", orderList);
         return "customerpage";
     }
 
--- LoginDemo/demo/src/main/java/comp31/demo/repo/OrderRepo.java
+++ LoginDemo/demo/src/main/java/comp31/demo/repo/OrderRepo.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import comp31.demo.model.User;
 import comp31.demo.model.order;
 
 @Repository
 public interface OrderRepo extends CrudRepository<order, Integer> {
+    List<order> findAll();
+    List<order> findByUserId(User user);
     //List<order> findbyOrderStatus(String orderStatus);
 }
